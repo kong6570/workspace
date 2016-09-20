589012ac5d7b687b323d0ac08c53e4967c78df88
zhangjingzhuan <619422869@qq.com>
2016-09-20 19:34:45 +0800

    plindromenumber

---
+++ leetcode/src/PalindromeNumber.java
@@ -0,0 +1,39 @@
+/**
+ * Created by zhangjingzhuan on 2016/9/20.
+ */
+public class PalindromeNumber {
+    public boolean isPalindrome(int x){
+        if(x == 0){
+            return true;
+        }else if(x == Integer.MAX_VALUE || x == Integer.MIN_VALUE){
+            return false;
+        }
+        if(x < 0){
+            return false;
+        }
+        //reverse int
+        StringBuilder reverseNumString = new StringBuilder();
+        int tempNum = x;
+
+        while(tempNum/10 > 0){
+            reverseNumString.append(tempNum%10);
+            tempNum = tempNum/10;
+        }
+        reverseNumString.append(tempNum);
+        long reverseNumLong = Long.valueOf(reverseNumString.toString()).longValue();
+        if(reverseNumLong > Integer.MAX_VALUE){
+            return  false;
+        }
+        if(reverseNumLong == x){
+            return true;
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int x = -2147447412;
+        PalindromeNumber test = new PalindromeNumber();
+        System.out.println(test.isPalindrome(x));
+    }
+}
